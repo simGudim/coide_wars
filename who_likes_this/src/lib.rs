a54bb0003d4e30166705dac1c660d39d4deb7d33
simGudim <simongudim@yahoo.ca>
2021-11-02 12:04:15 -0400

    added wholikesthis

---
+++ who_likes_this/src/lib.rs
@@ -0,0 +1,32 @@
+
+pub fn likes(names: &[&str]) -> String {
+    match names.len() {
+        0 => "no one likes this".to_owned(),
+        1 => format!("{} likes this", names[0]),
+        2 => format!("{} and {} like this", names[0], names[1]),
+        3 => format!("{}, {} and {} like this", names[0], names[1], names[2]),
+        _ => format!("{}, {} and {} others like this",names[0], names[1], names.len() - 2)
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::likes;
+
+    #[test]
+    fn example_tests() {
+        assert_eq!(likes(&[]), "no one likes this");
+        assert_eq!(likes(&["Peter"]), "Peter likes this");
+        assert_eq!(likes(&["Jacob", "Alex"]), "Jacob and Alex like this");
+        assert_eq!(
+            likes(&["Max", "John", "Mark"]),
+            "Max, John and Mark like this"
+        );
+        assert_eq!(
+            likes(&["Alex", "Jacob", "Mark", "Max"]),
+            "Alex, Jacob and 2 others like this"
+        );
+    }
+}
