196f7caee0bb811970d0e8e61f9d69b76bfc3fe1
simGudim <simongudim@yahoo.ca>
2021-11-04 12:23:26 -0400

    simple opt

--- steps_in_primes/src/lib.rs
+++ steps_in_primes/src/lib.rs
@@ -1,6 +1,11 @@
 
-pub fn is_prime(n: i64) -> bool {
-    for i in 2..(n - 1) {
+pub fn is_prime(n: u64) -> bool {
+    if n < 4 {
+        return true
+    } else if n % 2 == 0 {
+        return false
+    }
+    for i in 4..(n/2) {
         if n % i == 0 {
             return false
         }
@@ -9,20 +14,20 @@ pub fn is_prime(n: i64) -> bool {
 }
 
 
-pub fn step(g: i32, m: i64, n: i64) -> Option<(i64, i64)> {
-    let result = (0, 0);
+pub fn step(g: i32, m: u64, n: u64) -> Option<(u64, u64)> {
     let mut primes = vec![];
     for i in m..=n {
         if is_prime(i) {
             primes.push(i);
         }
     }
-    println!("{:?}", primes);
     if !primes.is_empty() {
-        for i in primes.clone().iter() {
-            let end = &primes.clone().into_iter().find(|x| i - x  == g as i64 || i -x == g as i64 * -1);
+        for i in 0..primes.len() {
+            let start: usize = if (i as i32 - g) < 0 { 0 } else { i - g as usize };
+            let end: usize = if (i + g as usize) > primes.len() - 1 { primes.len() - 1 } else { i + g as usize };
+            let end = &primes[start..end].into_iter().find(|&x| primes[i] as i64 - *x as i64  == g as i64 || primes[i] as i64 - *x as i64  == g as i64 * -1);
             if let Some(end) = end {
-                return Some((*i, *end))
+                return Some((primes[i], **end))
             }
         }
     }
@@ -30,7 +35,7 @@ pub fn step(g: i32, m: i64, n: i64) -> Option<(i64, i64)> {
 }
 
 
-pub fn testing(g: i32, m: i64, n: i64, exp: Option<(i64, i64)>) -> () {
+pub fn testing(g: i32, m: u64, n: u64, exp: Option<(u64, u64)>) -> () {
     assert_eq!(step(g, m, n), exp)
 }
 
@@ -44,11 +49,14 @@ mod tests {
 
     #[test]
     fn basics_step() {
-        // assert_eq!(is_prime(101), true);
         testing(2,100,110, Some((101, 103)));
-        // testing(4,100,110, Some((103, 107)));
-        // testing(8,30000,100000, Some((30089, 30097)));
-        // testing(11,30000,100000, None);
-        // testing(2,10000000,11000000, Some((10000139, 10000141)));
+        testing(4,100,110, Some((103, 107)));
+    }
+
+    #[test]
+    fn long_test() {
+        testing(8,30000,100000, Some((30089, 30097)));
+        testing(11,30000,100000, None);
+        testing(2,10000000,11000000, Some((10000139, 10000141)));
     }
 }
